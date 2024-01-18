f2d67c2fa86f087d061afd362ed445c79ad96b5a
Reiterovich <reiterovich.andriy@gmail.com>
2024-01-19 00:08:13 +0200

    2.1

--- src/redux/contact/contact.reducer.js
+++ src/redux/contact/contact.reducer.js
@@ -2,13 +2,13 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
+axios.defaults.baseURL = 'https://65a701c794c2c5762da62551.mockapi.io';
+
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkApi) => {
     try {
-      const data = await axios.get(
-        'https://65a701c794c2c5762da62551.mockapi.io/contacts'
-      );
+      const data = await axios.get('/contacts');
       return data;
     } catch (err) {
       return thunkApi.rejectWithValue(err.message);
@@ -20,12 +20,14 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contactData, thunkApi) => {
     try {
-      const data = await axios.post(
-        'https://65a701c794c2c5762da62551.mockapi.io/contacts/',
-        { name: contactData.name, phone: contactData.phone }
-      );
+      const data = await axios.post('/contacts/', {
+        name: contactData.name,
+        phone: contactData.phone,
+      });
+      Notiflix.Notify.success('Contact add successfully!');
       return data;
     } catch (err) {
+      Notiflix.Notify.failure(`Error: ${err.message}`);
       return thunkApi.rejectWithValue(err.message);
     }
   }
@@ -35,11 +37,11 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (idContact, thunkApi) => {
     try {
-      const data = await axios.delete(
-        `https://65a701c794c2c5762da62551.mockapi.io/contacts/${idContact}`
-      );
+      const data = await axios.delete(`/contacts/${idContact}`);
+      Notiflix.Notify.success('Contact deleted successfully!');
       return data;
     } catch (err) {
+      Notiflix.Notify.failure(`Error: ${err.message}`);
       return thunkApi.rejectWithValue(err.message);
     }
   }
@@ -80,29 +82,25 @@ const contactsSlice = createSlice({
         state.contacts.isLoading = false;
         state.contacts.error = payload;
       })
-      .addCase(addContact.pending, (state, { payload }) => {
+      .addCase(addContact.pending, state => {
         state.contacts.error = null;
       })
       .addCase(addContact.fulfilled, (state, { payload }) => {
-        Notiflix.Notify.success('Contact add successfully!');
         state.contacts.items = [...state.contacts.items, payload.data];
         console.log(payload.data);
       })
       .addCase(addContact.rejected, (state, { payload }) => {
-        Notiflix.Notify.failure(`Error: ${payload}`);
         state.contacts.error = payload;
       })
       .addCase(deleteContact.pending, state => {
         state.contacts.error = null;
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
-        Notiflix.Notify.success('Contact deleted successfully!');
         state.contacts.items = state.contacts.items.filter(
           contact => contact.id !== action.payload.data.id
         );
       })
       .addCase(deleteContact.rejected, (state, { payload }) => {
-        Notiflix.Notify.failure(`Error: ${payload}`);
         state.contacts.error = payload;
       }),
 });
